3e193275e66af50b0395f7a6bb45680f3ce66ad1
George G. Vega Yon <g.vegayon@gmail.com>
2018-06-05 16:09:36 -0400

    Last version of the simulations

---
+++ fig/bias_plots.r
@@ -0,0 +1,62 @@
+rm(list = ls())
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(magrittr)
+
+load("simulations/02-gold-standard/bias.rda")
+
+bias <- select(
+  bias, index, Prior, size_tag, miss_tag, PropLeafs_tag,
+  matches("_bias$")
+  ) %>%
+  as_tibble %>%
+  gather(
+    key = "parameter",
+    value = "bias",
+    which(grepl("(?<=bias)$", colnames(.), perl = TRUE))
+    ) %>%
+  mutate(parameter = gsub("_.+", "", parameter))
+
+#+ plotting, echo=TRUE
+# Plot -------------------------------------------------------------------------
+graphics.off()
+sizelvls <- levels(bias$size_tag)
+for (i in 1:4) {
+  # Clearing plot space and creating the pdf
+  
+  pdf(sprintf("fig/bias_trees_of_size_%s.pdf", switch (i,
+    `1` = "small",
+    `2` = "mid-small",
+    `3` = "mid-large",
+    `4` = "large"
+  )))
+  
+  # Nobservations in this group
+  nobs <- bias %>% dplyr::filter(as.integer(size_tag) == i) %>%
+    subset(select=index) %>% unique %>% nrow
+  
+  
+  # Creating the plot
+  p <- bias %>% dplyr::filter(as.integer(size_tag) == i) %>%
+    # Creating the boxplot
+    ggplot(aes(parameter, bias)) + geom_boxplot(aes(colour = Prior)) +
+    
+    # Adding an horizontal line, and spliting my % of missings
+    geom_hline(yintercept = 0, lty=2) + facet_grid(miss_tag ~ .) +
+    ylim(-.2,.2) + ylab("Bias") + xlab("")
+  
+  # Adding a title
+  message(
+    sprintf("Bias distribution for trees of size %s", levels(bias$size_tag)[i]),
+    subtitle = sprintf("# of observations: %i", nobs)
+  )
+  
+  # Printing it on screen (need to do that explicitly on a loop)
+  print(p)
+  message("Level ", levels(bias$size_tag)[i], " done.")
+  
+  dev.off()
+  
+}
---
+++ simulations/01-informative-force/coverage_probabilities.r
@@ -0,0 +1,72 @@
+rm(list = ls())
+
+library(dplyr)
+library(magrittr)
+library(xtable)
+
+# Setting xtable options
+options(xtable.booktabs = TRUE)
+options(xtable.include.rownames = FALSE)
+options(xtable.sanitize.text.function = function(x) x)
+
+load("simulations/01-informative-force/bias.rda")
+
+# Cleaning latex errors
+# See https://www.sharelatex.com/learn/Errors/Illegal_unit_of_measure_(pt_inserted)
+bias <- bias %>%
+  mutate(
+    size_tag = paste0("{", size_tag, "}"),
+    miss_tag = paste0("{", miss_tag, "}")
+  )
+
+# Coverage probability
+summ <- bias %>% 
+  group_by(Prior, size_tag, miss_tag) %>%
+  summarize(
+    psi0 = mean(psi0_covered95),
+    psi1 = mean(psi1_covered95),
+    mu0 = mean(mu0_covered95),
+    m1 = mean(mu1_covered95),
+    Pi = mean(Pi_covered95)
+    ) %>%
+  arrange(Prior, size_tag, miss_tag)
+
+fact <- summ$Prior
+summ <- summ %>% ungroup %>%
+  select(-Prior) %>%
+  rename(
+    Missing = miss_tag,
+    Size    = size_tag
+    ) %>%
+  split(., fact) %>%
+  `attr<-`("subheadings", paste(names(.), "Prior")) %>%
+  xtableList(
+    caption = "Coverage probability at the 95\\% level by prior (right/wrong), size of the tree, and proportion of missingness.  Estimations with the \\emph{right} prior use the same priors as the data generating process, whereas estimations with the \\emph{wrong} prior used a prior that had a mean twice as large as the data generating process.",
+    label   = "tab:coverage95-method-size-missigness") %>%
+  print %>%
+  cat(file = "tables/01_coverage95_by_method_size_missingness.tex")
+
+
+summ <- bias %>% 
+  group_by(Prior, miss_tag) %>%
+  summarize(
+    psi0 = mean(psi0_covered95),
+    psi1 = mean(psi1_covered95),
+    mu0 = mean(mu0_covered95),
+    m1 = mean(mu1_covered95),
+    Pi = mean(Pi_covered95)
+  ) %>%
+  arrange(Prior, miss_tag)
+
+fact <- summ$Prior
+summ %>% ungroup %>%
+  select(-Prior) %>%
+  rename(Missing = miss_tag) %>%
+  split(., fact) %>%
+  `attr<-`("subheadings", paste(names(.), "Prior")) %>%
+  xtableList(
+    caption = "Coverage probability at the 95\\% level by prior (right/wrong), and proportion of missingness. Estimations with the \\emph{right} prior use the same priors as the data generating process, whereas estimations with the \\emph{wrong} prior used a prior that had a mean twice as large as the data generating process.",
+    label   = "tab:coverage95-method-missigness") %>%
+  print %>%
+  cat(file = "tables/01_coverage95_by_method_missingness.tex")
+
