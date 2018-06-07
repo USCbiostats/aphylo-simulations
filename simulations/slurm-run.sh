fe53a2d9b77a5b8764bc00d80a6eb230296cd39e
George G. Vega Yon <g.vegayon@gmail.com>
2018-06-07 15:32:54 -0700

    slurmme

--- simulations/slurm-run.sh
+++ simulations/slurm-run.sh
@@ -3,7 +3,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=4
 #SBATCH --ntasks-per-core=1
-SLURM_SUBMIT_DIR = /home/rcf-proj/pdt/vegayon/aphylo-simulations/simulations
-#SBATCH --test-only
+##SLURM_SUBMIT_DIR = /home/rcf-proj/pdt/vegayon/aphylo-simulations/simulations
+##SBATCH --test-only
 
-R CMD BATCH dgp.r dgp.rout
+export RPROJECT=/home/rcf-proj/pdt/vegayon/aphylo-simulations
+export PANTHER=/auto/pmd-02/pdt/pdthomas/panther/famlib/rel/PANTHER13.1\_altVersion/hmmscoring/PANTHER13.1
+export R_LIBS=~/R/x86_64-redhat-linux-gnu-library/3.4/
+
+/usr/usc/R/3.4.0/bin/Rscript --vanilla dgp.r
