52e027736589a290229dd9a0bba9d41071dedde8
George G. Vega Yon <g.vegayon@gmail.com>
2019-01-17 17:16:44 -0500

    Function to read candidate trees

---
+++ global-paths.r
@@ -0,0 +1,3 @@
+STAGING_PATH <- "/staging/pdt/vegayon/aphylo-simulations"
+PANTHER_PATH <- "/auto/pmd-02/pdt/pdthomas/panther/famlib/rel/PANTHER13.1_altVersion/hmmscoring/PANTHER13.1/books"
+PROJECT_PATH <- "/home/rcf-proj2/pdt/vegayon/aphylo-simulations"
