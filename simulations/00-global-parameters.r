758dce287bf1444ec730110527d09c542d19f89d
George G. Vega Yon <g.vegayon@gmail.com>
2019-01-24 00:55:49 -0800

    Adding cross validation script

--- simulations/00-global-parameters.r
+++ simulations/00-global-parameters.r
@@ -1,5 +1,5 @@
 # Local paths
-source("../global-paths.r")
+source("global-paths.r")
 # STAGING_PATH <- "/staging/pdt/vegayon/aphylo-simulations"
 # PANTHER_PATH <- "/auto/pmd-02/pdt/pdthomas/panther/famlib/rel/PANTHER13.1_altVersion/hmmscoring/PANTHER13.1/books"
 # PROJECT_PATH <- "/home/rcf-proj2/pdt/vegayon/aphylo-simulations"
