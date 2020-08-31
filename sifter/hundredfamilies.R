c9e9cf092f52b4ff15f609bf0a7dcc7863c541cd
George G Vega Yon <g.vegayon@gmail.com>
2020-08-31 09:37:08 -0700

    Saving the genes info

--- sifter/hundredfamilies.R
+++ sifter/hundredfamilies.R
@@ -4,7 +4,7 @@ library(ape)
 library(aphylo)
 
 # Reading trees
-fn_nhx <- list.files("sifter/hundred/", pattern = "nhx", full.names = TRUE)
+fn <- list.files("sifter/hundred/", pattern = "nhx", full.names = TRUE)
 tn <- gsub("(.+/)([a-zA-Z0-9]+)\\.nhx", "\\2", fn, perl = TRUE)
 
 trees <- parallel::mclapply(fn, read.tree, mc.cores = 4L)
@@ -15,5 +15,10 @@ info  <- parallel::mclapply(fn, function(i) {
 
 names(trees) <- tn
 
-# Reading functional annotations
-fn_pli
+# Building a data set to exctract the information from uniprot
+genes <- data.frame(
+  tree = rep(tn, sapply(trees, Ntip)),
+  gene = unlist(sapply(trees, "[[", "tip.label"))
+)
+
+write.csv2(genes, file = "sifter/hundredfamilies_genes.csv", row.names = FALSE)
