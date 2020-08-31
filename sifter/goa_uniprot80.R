9e555e4a2a5ff3bfbc3179c348738059f852f15e
George G Vega Yon <g.vegayon@gmail.com>
2020-08-31 16:26:51 -0700

    Right match

--- sifter/goa_uniprot80.R
+++ sifter/goa_uniprot80.R
@@ -7,10 +7,17 @@ goa <- fread("sifter/gene_association.goa_uniprot.80", header = FALSE)
 cnames <- readLines("sifter/gaf_1.0_colnames.txt")
 colnames(goa) <- cnames
 
-# Reading hundred fams
-fams <- fread("sifter/hundredfamilies_annotations.csv.gz")
+# Keeping only relevant
+codes <- c("IDA", "IMP", "TAS")
+goa <- goa[Evidence_Code %in% codes]
 
-dat <- merge(x = fams, y = goa, by.y = "DB_Object_ID", by.x = "number",
+# ID Synonim
+goa[, synm := gsub(".+[|]([a-zA-Z0-9_]+)$", "\\1", DB_Object_Synonym, perl = TRUE)]
+
+# Reading hundred fams
+fams <- fread("sifter/hundredfamilies_annotations.csv")
+
+dat <- merge(x = fams, y = goa, by.y = "synm", by.x = "number",
   all.x = TRUE, all.y = FALSE)
 
 saveRDS(dat, "sifter/goa_uniprot80.rds", compress = TRUE)
