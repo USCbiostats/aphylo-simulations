fb18d34a46006c072ae688e3698f557b5a66b9b3
George G Vega Yon <g.vegayon@gmail.com>
2020-09-01 17:00:30 -0700

    Working on a regex to get the NHX

--- sifter/goa_uniprot80.R
+++ sifter/goa_uniprot80.R
@@ -22,3 +22,29 @@ dat <- merge(x = fams, y = goa, by.y = "synm", by.x = "number",
 
 saveRDS(dat, "sifter/goa_uniprot80.rds", compress = TRUE)
 
+goterms <- strsplit(fams[go != ""]$go, split = ",")
+goterms <- gsub("\\s+|\\[|\\]", "", unlist(goterms))
+length(goterms)
+nrow(fams[go != ""])
+
+table(goterms)
+
+dat_wide <- dat[GO_ID %in% paste0("GO:", goterms)]
+dat_wide <- dcast(dat_wide, number + famid ~ Qualifier + GO_ID)
+
+library(aphylo)
+
+set.seed(3)
+tree0 <- raphylo(100, psi = c(0,0), mu_d=c(.95, .5), mu_s = c(.05, .05), Pi = .01)
+# plot(tree0)
+
+tree <- rdrop_annotations(tree0, .9, prob.drop.0 = .9)
+plot(tree)
+
+pred <- predict_pre_order(tree, psi = c(0,0), mu_d=c(.95, .5), mu_s = c(.05, .05), Pi = .01, eta = c(1, 1))
+
+pred <- pred[1:Ntip(tree0),,drop=FALSE]
+
+auc(pred, tree0$tip.annotation)
+tree0$tip.annotation <- cbind(tree0$tip.annotation, tree$tip.annotation, pred = pred)
+plot(tree0)
--- sifter/hundredfamilies.R
+++ sifter/hundredfamilies.R
@@ -4,7 +4,7 @@ library(ape)
 library(aphylo)
 
 # Reading trees
-fn <- list.files("sifter/sifter/100trees/", pattern = "nhx", full.names = TRUE)
+fn <- list.files("sifter/hundred/", pattern = "nhx", full.names = TRUE)
 tn <- gsub("(.+/)([a-zA-Z0-9]+)\\.nhx", "\\2", fn, perl = TRUE)
 
 trees <- parallel::mclapply(fn, read.tree, mc.cores = 4L)
@@ -12,6 +12,25 @@ info  <- parallel::mclapply(fn, function(i) {
   x <- readLines(i)
   stringr::str_extract_all(x, "\\[:\\][0-9\\.]+\\[&&NHX:[A-Z][:=][A-Z]\\]")
 }, mc.cores = 4L)
+
+pf04988 <- readLines("sifter/hundred/PF04988.nhx")
+p1 <- "((B:0.2,(C:0.3,D:0.4)E:0.5)A:0.1[&&NHX:AA=1:B=2])F;"
+
+p <- p1
+
+pattern <- "([a-zA-Z0-9_]*)([:][0-9]+[.]?[0-9]*)(\\[&&NHX([:][a-zA-Z0-9_]+[=][a-zA-Z0-9_]+)*\\])?"
+
+# https://regex101.com/r/oDp844/1
+# ([a-zA-Z0-9_]*)([:][0-9]+[.]?[0-9]*)(\[&&NHX[a-zA-Z0-9_:=]+\])?
+x       <- gregexpr(p, pattern = pattern, perl = TRUE)
+x       <- regmatches(p, x)
+
+regmatches(x[[1]], regexec(x[[1]], pattern = pattern, perl = TRUE))
+
+read_nhx <- function(x) {
+  
+}
+
 
 names(trees) <- tn
 
