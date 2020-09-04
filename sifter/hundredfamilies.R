fbf2e3773e868560561a55f54e24b3fb6f0283c0
George G Vega Yon <g.vegayon@gmail.com>
2020-09-04 16:08:15 -0700

    New Hampshire eXtended format working OK

--- sifter/hundredfamilies.R
+++ sifter/hundredfamilies.R
@@ -2,6 +2,7 @@
 
 library(ape)
 library(aphylo)
+library(stringi)
 
 # Reading trees
 fn <- list.files("sifter/hundred/", pattern = "nhx", full.names = TRUE)
@@ -14,8 +15,8 @@ read_nhx <- function(text) {
   pattern <- "([(,)])([a-zA-Z0-9_]*)([:][0-9]+[.]?[0-9]*)(\\[&&NHX[a-zA-Z0-9_:=]+\\])?"
   
   # Capturing the patterns and splitting the data
-  x       <- gregexpr(text, pattern = pattern, perl = TRUE)
-  x       <- regmatches(text, x)
+  x <- gregexpr(text, pattern = pattern, perl = TRUE)
+  x <- regmatches(text, x)
   
   # Creating a matrix with the data
   x <- regmatches(x[[1]], regexec(x[[1]], pattern = pattern, perl = TRUE))
@@ -27,7 +28,6 @@ read_nhx <- function(text) {
     x[noid,3] <- sprintf("unnamed%04i", 1:length(noid))
   }
   
-  # Replacing the ones with empty id
   for (i in noid) {
     text <- sub(
       pattern     = x[i,1],
@@ -36,7 +36,7 @@ read_nhx <- function(text) {
       fixed       = TRUE
       )
   }
-  
+
   # Is there any root?
   text <- sub(
     pattern = "[)][;]$", replacement = ")root;", x = text, perl = TRUE
@@ -45,16 +45,42 @@ read_nhx <- function(text) {
   dat <- x[,-c(1L, 2L)]
   
   # Capturing NHX fields
-
+  nhx <- strsplit(dat[,3], split = "[:]|[=]")
+  nhx <- tryCatch(lapply(nhx, function(n) {
+    if (length(n) > 0) {
+      n <- gsub(pattern = "(^\\[|\\]$)", replacement = "", x = n)
+      n <- matrix(n[-1], ncol = 2, byrow = TRUE)
+      structure(.Data = n[,1], names = n[,2])
+    } else
+     n
+  }), error = function(e) e)
+  
+  if (inherits(nhx, "error")) 
+    stop(
+      "There was a problem when processing the &&NHS blocks.",
+      " Possibly, not all the attributes have the right tag. Here is the error",
+      ":\n", paste0(nhx, collapse=""), call. = FALSE
+      )
+  
   list(
     tree = ape::read.tree(text = text),
     edge = dat[,-3L],
-    nhx  = strsplit(dat[,3L], split=":")
+    nhx  = nhx
   )
   
 }
 
+x <- read_nhx("sifter/hundred/PF00610.nhx")
+
+microbenchmark::microbenchmark(
+  stringi = read_nhx("sifter/hundred/PF00610.nhx", wstringi = TRUE),
+  gsub    = read_nhx("sifter/hundred/PF00610.nhx", wstringi = FALSE)
+)
+
+stop()
+
 trees <- parallel::mclapply(fn, read_nhx, mc.cores = 4L)
+
 info  <- parallel::mclapply(fn, function(i) {
   x <- readLines(i)
   stringr::str_extract_all(x, "\\[:\\][0-9\\.]+\\[&&NHX:[A-Z][:=][A-Z]\\]")
